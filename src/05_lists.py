0189052215e697296a7e37f0bf0b0493e4561aa5
Juan Aleman <juan.j.aleman@gmail.com>
2020-07-08 23:09:31 -0500

    completed 05_lists

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,34 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+# del x[4] #one way of doing it I think this is for a specific element at a specific index
+x.remove(8) 
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for (index, element) in enumerate(x):
+    print(f'{x[index] * 1000}')
+
+#another way of doing this ^ only this created another list
+x_multi = [ num*1000 for num in x]
+print(x_multi)
